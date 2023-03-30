8a021d8e3cbf8277620647882b9e884d57fd3e4a
dhiyarhmn <100927176+dhiyarhmn@users.noreply.github.com>
2023-03-31 00:52:11 +0700

    Update IArea.java

--- Praktikum 5/IArea.java
+++ Praktikum 5/IArea.java
@@ -5,7 +5,7 @@ package org.bangundatar;
 * Nama      : Puti Dhiya Salsabila Rahman
 * NIM       : 24060121140173
 * Tanggal   : 27/03/2023
-* Deskripsi : Implementasi dari interface ini harus memiliki cara untuk menghitung luas
+* Deskripsi : interface yang mendefinisikan kontrak bahwa setiap kelas yang membuat implementasi dari interface ini harus memiliki cara untuk menghitung luas
 * Lab		: B1
 */
 
