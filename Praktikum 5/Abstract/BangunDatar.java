851491bfed4d4666d41bcf4817162ccd6758ea13
dhiyarhmn <100927176+dhiyarhmn@users.noreply.github.com>
2023-03-31 11:51:28 +0700

    Create BangunDatar.java

---
+++ Praktikum 5/Abstract/BangunDatar.java
@@ -0,0 +1,25 @@
+/*
+* Nama File : BangunDatar.java
+* Nama      : Puti Dhiya Salsabila Rahman
+* NIM       : 24060121140173
+* Tanggal   : 27/03/2023
+* Deskripsi: kelas abstrak, berisi abstraksi bangun datar
+* Lab		: B1
+*/
+
+package org.bangundatar.BangungDatar;
+
+public abstract class BangunDatar{
+	
+	protected double luas;
+	
+	public abstract double hitungLuas (double sisi);
+	
+	public void setLuas (double a) {
+		luas = a;
+	}
+
+	public double getLuas (){
+		return luas;
+	}
+}
