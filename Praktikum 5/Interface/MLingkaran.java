19651dec4e96f253902e041f810e4023a51c2d2b
dhiyarhmn <100927176+dhiyarhmn@users.noreply.github.com>
2023-03-31 12:05:55 +0700

    Create MLingkaran.java

---
+++ Praktikum 5/Interface/MLingkaran.java
@@ -0,0 +1,24 @@
+/*
+* Nama File : MLingkaran.java
+* Nama      : Puti Dhiya Salsabila Rahman
+* NIM       : 24060121140173
+* Tanggal   : 27/03/2023
+* Deskripsi : Kelas implementasi IArea berupa cara menghitung luas lingkaran
+* Lab		: B1
+*/
+
+import java.util.Scanner;
+import org.bangundatar.Lingkaran;
+
+public class MLingkaran {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jari jari lingkaran: ");
+        double jejari = input.nextDouble();
+
+        Lingkaran lingkaran = new Lingkaran(jejari);
+        System.out.println("Luas Lingkaran dengan jejer " + jejari + " adalah " + lingkaran.hitungLuas());
+
+        input.close();
+    }
+}
