d110204180899e10cb547eabe17826fea30dd520
dhiyarhmn <100927176+dhiyarhmn@users.noreply.github.com>
2023-03-31 19:52:37 +0700

    Update BujurSangkar.java

--- Praktikum 5/Abstract/BujurSangkar.java
+++ Praktikum 5/Abstract/BujurSangkar.java
@@ -3,14 +3,16 @@
 * Nama      : Puti Dhiya Salsabila Rahman
 * NIM       : 24060121140173
 * Tanggal   : 27/03/2023
-*Deskripsi: kelas yang membuat implementasi metode abstrak pada bangun datar
-* Lab		: B1
+* Deskripsi : kelas yang membuat implementasi metode abstrak pada bangun datar
+* Lab	    : B1
 */
 
-import bangundatar.BangunDatar;
-public class BujurSangkar extends BangunDatar{
-	public double hitungLuas (double sisi) {
-		luas = sisi*sisi;
+public class BujurSangkar extends BangunDatar {
+	public double hitungLuas(double sisi) {
+		luas = sisi * sisi;
 		return luas;
 	}
 }
+
+//Apabila BujurSangkar tidak membuat implementasi metode abstrak pada kelas BangunDatar, maka kelas BujurSangkar harus dijadikan kelas abstrak juga
+//Jika tidak dijadikan kelas abstrak maka akan terjadi kesalahan kompilasi dan error 
