a400031cd160a425c9c11c046f3fbb8d783ac609
dhiyarhmn <100927176+dhiyarhmn@users.noreply.github.com>
2023-03-31 11:53:41 +0700

    Create BujurSangkar.java

---
+++ Praktikum 5/Abstract/BujurSangkar.java
@@ -0,0 +1,16 @@
+/*
+* Nama File : BujurSangkar.java
+* Nama      : Puti Dhiya Salsabila Rahman
+* NIM       : 24060121140173
+* Tanggal   : 27/03/2023
+*Deskripsi: kelas yang membuat implementasi metode abstrak pada bangun datar
+* Lab		: B1
+*/
+
+import bangundatar.BangunDatar;
+public class BujurSangkar extends BangunDatar{
+	public double hitungLuas (double sisi) {
+		luas = sisi*sisi;
+		return luas;
+	}
+}
