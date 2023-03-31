5e44ffcc1c369967f60fe0e76a5a9885bf9e0a0c
dhiyarhmn <100927176+dhiyarhmn@users.noreply.github.com>
2023-03-31 19:49:24 +0700

    Update MBujurSangkar.java

--- Praktikum 5/Abstract/MBujurSangkar.java
+++ Praktikum 5/Abstract/MBujurSangkar.java
@@ -3,7 +3,7 @@
 * Nama      : Puti Dhiya Salsabila Rahman
 * NIM       : 24060121140173
 * Tanggal   : 27/03/2023
-* Deskripsi : kelas abstrak, berisi abstraksi bangun datar
+* Deskripsi : Kelas yang mengimplementasikan cara menghitung luas bujur sangkar
 * Lab	    : B1
 */
 
